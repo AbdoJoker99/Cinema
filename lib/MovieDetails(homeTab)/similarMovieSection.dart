0d6942c4188502f81cdcf36e7f6d11f2636f9391
AbdoJoker99 <chessjoker27@gmail.com>
2024-10-18 02:57:31 +0300

    finalProject_ui_updates

--- lib/MovieDetails(homeTab)/similarMovieSection.dart
+++ lib/MovieDetails(homeTab)/similarMovieSection.dart
@@ -17,7 +17,7 @@ class SimilarMoviesSection extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: EdgeInsets.all(17.w),
+          padding: EdgeInsets.symmetric(horizontal: 17.w, vertical: 10.h),
           child: Text(
             'More Like This',
             style: TextStyle(
@@ -29,7 +29,7 @@ class SimilarMoviesSection extends StatelessWidget {
         ),
         if (similarMovies != null && similarMovies!.isNotEmpty)
           Container(
-            height: 201.h,
+            height: 210.h,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: similarMovies!.length,
@@ -38,49 +38,94 @@ class SimilarMoviesSection extends StatelessWidget {
                 return Padding(
                   padding: EdgeInsets.only(left: 17.w),
                   child: GestureDetector(
-                    // Wrap with GestureDetector
                     onTap: () {
                       // Handle tap to navigate to movie details
                       Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MovieDetailsScreen(
-                                  movieId: similarMovies![index].id!.toInt())));
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.network(
-                          similarMovie.posterPath != null
-                              ? 'https://image.tmdb.org/t/p/w500${similarMovie.posterPath}'
-                              : 'https://via.placeholder.com/121x160?text=No+Image',
-                          width: 120.w,
-                          height: 140.h,
-                          fit: BoxFit.cover,
-                          errorBuilder: (context, error, stackTrace) {
-                            return Center(
-                              child: Text('Image failed to load'),
-                            );
-                          },
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MovieDetailsScreen(
+                            movieId: similarMovie.id!.toInt(),
+                          ),
                         ),
-                        SizedBox(height: 9.h),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 17,
-                            ),
-                            SizedBox(width: 5.w),
-                            Text(
-                              similarMovie.voteAverage?.toString() ??
-                                  "No Rating",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 15.sp,
+                      );
+                    },
+                    child: Stack(
+                      children: [
+                        // Movie Poster
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            similarMovie.posterPath != null
+                                ? 'https://image.tmdb.org/t/p/w500${similarMovie.posterPath}'
+                                : 'https://via.placeholder.com/121x160?text=No+Image',
+                            width: 120.w,
+                            height: 160.h,
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) {
+                              return Container(
+                                width: 120.w,
+                                height: 160.h,
+                                color: Colors.grey,
+                                child: Center(
+                                  child: Text(
+                                    'Image not available',
+                                    style: TextStyle(color: Colors.white),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        // Rating (Stars) Positioned at the top
+                        Positioned(
+                          top: 160.h,
+                          left: 10.w,
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                                size: 17,
                               ),
-                            ),
-                          ],
+                              SizedBox(width: 5.w),
+                              Text(
+                                similarMovie.voteAverage?.toString() ??
+                                    "No Rating",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14.sp,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8.h,
+                        ),
+                        // Movie Title Positioned at the bottom
+                        Positioned(
+                          top: 180.h,
+                          bottom: 0.h,
+                          left: 10.w,
+                          right: 10.w,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                similarMovie.title ?? "",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleSmall!
+                                    .copyWith(
+                                      color: Colors.white,
+                                      fontSize: 12.sp,
+                                    ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
@@ -115,8 +160,20 @@ class MovieDetailsPage extends StatelessWidget {
     // Replace this with your actual details page layout
     return Scaffold(
       appBar: AppBar(title: Text(movie.title ?? 'Movie Details')),
-      body: Center(
-        child: Text('Details for ${movie.title}'),
+      body: Padding(
+        padding: EdgeInsets.all(16.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              movie.title ?? "Unknown title",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            SizedBox(height: 10.h),
+            Text('Release Date: ${movie.releaseDate ?? "Unknown"}'),
+            // Add more fields and layout according to movie details here
+          ],
+        ),
       ),
     );
   }
